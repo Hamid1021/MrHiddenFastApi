35ebea33e91c1c77ffe98a0ce937ded0cbc73ac6
HamidReza <hamid10631021@hotmail.com>
2024-12-30 23:55:21 +0330

    ایحاد دیتا بیس و جدول ها بر اساس دستورات مایگریشن المبیک

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from config.db_config import create_db_and_tables, engine
 from routers import user_router, blog_router
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    create_db_and_tables()
-    yield
-    await engine.dispose()
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     create_db_and_tables()
+#     yield
+#     await engine.dispose()
 
-app = FastAPI(lifespan=lifespan)
+# app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.include_router(user_router.router)
 app.include_router(blog_router.router)
